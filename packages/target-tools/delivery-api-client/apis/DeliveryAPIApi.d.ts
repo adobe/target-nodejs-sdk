96b5744cf5c87f801af8ccce04a88495cf82363d
Manjiri Tapaswi <tapaswi@adobe.com>
2020-12-08 15:53:47 -0800

    Regenerated delivery api specs

--- packages/target-tools/delivery-api-client/apis/DeliveryAPIApi.d.ts
+++ packages/target-tools/delivery-api-client/apis/DeliveryAPIApi.d.ts
@@ -1,12 +1,12 @@
 import * as runtime from '../runtime';
 import { DeliveryRequest, DeliveryResponse } from '../models';
 export interface ExecuteDeliveryRequest {
-    imsOrgId: string;
+    client: string;
     sessionId: string;
     deliveryRequest: DeliveryRequest;
     version?: string;
 }
 export declare class DeliveryAPIApi extends runtime.BaseAPI {
     executeRaw(requestParameters: ExecuteDeliveryRequest): Promise<runtime.ApiResponse<DeliveryResponse>>;
-    execute(imsOrgId: string, sessionId: string, deliveryRequest: DeliveryRequest, version?: string): Promise<DeliveryResponse>;
+    execute(client: string, sessionId: string, deliveryRequest: DeliveryRequest, version?: string): Promise<DeliveryResponse>;
 }
