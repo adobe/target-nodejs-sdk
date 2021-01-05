bb2071d73b6fbce626db1b5c00b882b0becefea0
Jason Waters <waters@adobe.com>
2021-01-05 11:06:57 -0800

    allocationProvider.spec.js

--- packages/target-decisioning-engine/src/allocationProvider.spec.js
+++ packages/target-decisioning-engine/src/allocationProvider.spec.js
@@ -134,7 +134,6 @@ describe("allocationProvider", () => {
         ]
       });
 
-      // tntId generated
       expect(typeof vid).toEqual("string");
       expect(vid.length).toBeGreaterThan(0);
 
